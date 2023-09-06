609807326cebdb15e512c9581e2f79f28646bcee
Djx990216 <903109540@qq.com>
2023-09-06 20:43:38 +0800

    lock

---
+++ src/muitiThreadExer/LockTest.java
@@ -0,0 +1,57 @@
+package muitiThreadExer;
+
+/*
+* 使用Lock锁解决线程安全问题  ---jdk5.0新增
+* */
+
+import java.util.concurrent.locks.ReentrantLock;
+
+class Window2 implements Runnable{
+
+    //票
+    private int ticket = 100;
+
+    //锁对象
+    private ReentrantLock lock = new ReentrantLock();
+
+    @Override
+    public void run() {
+        while (true){
+            try {
+
+                //上锁
+                lock.lock();
+
+                if (ticket > 0) {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println(Thread.currentThread().getName() + ": 第" + ticket + "张票已售出");
+                    ticket--;
+                } else {
+                    break;
+                }
+            }finally {
+                //解锁
+                lock.unlock();
+            }
+        }
+    }
+}
+
+public class LockTest {
+    public static void main(String[] args) {
+        Window2 w = new Window2();
+        Thread t1 = new Thread(w);
+        Thread t2 = new Thread(w);
+        Thread t3 = new Thread(w);
+        t1.setName("窗口1");
+        t2.setName("窗口2");
+        t3.setName("窗口3");
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
