632a14c2a01990ea17999fa384dd6108edd0b773
Djx990216 <903109540@qq.com>
2023-09-11 21:15:21 +0800

    Test3

--- StringTest/src/StringProblem/StringExer.java
+++ StringTest/src/StringProblem/StringExer.java
@@ -43,4 +43,25 @@ public class StringExer {
         String res = new String(arr);
         System.out.println(res);
     }
+
+    //统计一个字符串在另一个字符串中出现的次数
+    @Test
+    public void Test3(){
+        String str = "abkkcadkabkebfkabkskab";
+        String target = "ab";
+        int counter = 0;
+        String substr = str;
+        int subLen = str.length();
+        while(substr.indexOf(target) != -1){
+            counter++;
+            int pos = substr.indexOf(target);
+            int next = pos + target.length();
+            if(next >= subLen){
+                break;
+            }
+            substr = substr.substring(next, subLen);
+            subLen = subLen - next;
+        }
+        System.out.println("目标字符串出现的次数是：" + counter + "次");
+    }
 }
